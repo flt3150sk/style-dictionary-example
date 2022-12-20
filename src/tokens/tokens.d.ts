4b6e884e6d6277eac25419983ea176869ea4abad
flt3150sk <muranaka.kouske@gmail.com>
2022-12-21 00:55:36 +0900

    add sample

---
+++ src/tokens/tokens.d.ts
@@ -0,0 +1,59 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 20 Dec 2022 14:53:17 GMT
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value: any;
+  name?: string;
+  comment?: string;
+  themeable?: boolean;
+  attributes?: {
+    category?: string;
+    type?: string;
+    item?: string;
+    subitem?: string;
+    state?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+declare const tokens: {
+  "border-radius-4": DesignToken;
+  "border-radius-8": DesignToken;
+  "border-radius-12": DesignToken;
+  "border-radius-24": DesignToken;
+  "border-radius-full": DesignToken;
+  "Green 1000": DesignToken;
+  "Green 800": DesignToken;
+  "Green 600": DesignToken;
+  "Green 400": DesignToken;
+  "Green 200": DesignToken;
+  "Whtie 1000": DesignToken;
+  "Gray 1000": DesignToken;
+  "Gray 800": DesignToken;
+  "Gray 600": DesignToken;
+  "Gray 400": DesignToken;
+  "Gray 200": DesignToken;
+  "font-size-10": DesignToken;
+  "font-size-12": DesignToken;
+  "font-size-14": DesignToken;
+  "font-size-16": DesignToken;
+  "font-size-20": DesignToken;
+  "font-size-24": DesignToken;
+  "font-size-32": DesignToken;
+  "font-family-base": DesignToken;
+  "opacity-20": DesignToken;
+  "opacity-50": DesignToken;
+  "opacity-80": DesignToken;
+  "border-width-1": DesignToken;
+  "border-width-2": DesignToken;
+  "border-width-3": DesignToken;
+  "border-width-4": DesignToken;
+  tokenSetOrder: {
+    "0": DesignToken;
+  };
+};
---
+++ src/tokens/tokens.js
@@ -0,0 +1,555 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 20 Dec 2022 14:53:17 GMT
+ */
+
+module.exports = {
+  "border-radius-4": {
+    "value": "4px",
+    "type": "borderRadius",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "4px",
+      "type": "borderRadius"
+    },
+    "name": "BorderRadius4",
+    "attributes": {
+      "category": "border-radius-4"
+    },
+    "path": [
+      "border-radius-4"
+    ]
+  },
+  "border-radius-8": {
+    "value": "8px",
+    "type": "borderRadius",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "8px",
+      "type": "borderRadius"
+    },
+    "name": "BorderRadius8",
+    "attributes": {
+      "category": "border-radius-8"
+    },
+    "path": [
+      "border-radius-8"
+    ]
+  },
+  "border-radius-12": {
+    "value": "12px",
+    "type": "borderRadius",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "12px",
+      "type": "borderRadius"
+    },
+    "name": "BorderRadius12",
+    "attributes": {
+      "category": "border-radius-12"
+    },
+    "path": [
+      "border-radius-12"
+    ]
+  },
+  "border-radius-24": {
+    "value": "24px",
+    "type": "borderRadius",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "24px",
+      "type": "borderRadius"
+    },
+    "name": "BorderRadius24",
+    "attributes": {
+      "category": "border-radius-24"
+    },
+    "path": [
+      "border-radius-24"
+    ]
+  },
+  "border-radius-full": {
+    "value": "9999px",
+    "type": "borderRadius",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "9999px",
+      "type": "borderRadius"
+    },
+    "name": "BorderRadiusFull",
+    "attributes": {
+      "category": "border-radius-full"
+    },
+    "path": [
+      "border-radius-full"
+    ]
+  },
+  "Green 1000": {
+    "value": "#3B6B10",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#3B6B10",
+      "type": "color"
+    },
+    "name": "Green1000",
+    "attributes": {
+      "category": "Green 1000"
+    },
+    "path": [
+      "Green 1000"
+    ]
+  },
+  "Green 800": {
+    "value": "#849E6D",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#849E6D",
+      "type": "color"
+    },
+    "name": "Green800",
+    "attributes": {
+      "category": "Green 800"
+    },
+    "path": [
+      "Green 800"
+    ]
+  },
+  "Green 600": {
+    "value": "#84B312",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#84B312",
+      "type": "color"
+    },
+    "name": "Green600",
+    "attributes": {
+      "category": "Green 600"
+    },
+    "path": [
+      "Green 600"
+    ]
+  },
+  "Green 400": {
+    "value": "#E7EFDF",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#E7EFDF",
+      "type": "color"
+    },
+    "name": "Green400",
+    "attributes": {
+      "category": "Green 400"
+    },
+    "path": [
+      "Green 400"
+    ]
+  },
+  "Green 200": {
+    "value": "#F8FAF5",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#F8FAF5",
+      "type": "color"
+    },
+    "name": "Green200",
+    "attributes": {
+      "category": "Green 200"
+    },
+    "path": [
+      "Green 200"
+    ]
+  },
+  "Whtie 1000": {
+    "value": "#FFFFFF",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#FFFFFF",
+      "type": "color"
+    },
+    "name": "Whtie1000",
+    "attributes": {
+      "category": "Whtie 1000"
+    },
+    "path": [
+      "Whtie 1000"
+    ]
+  },
+  "Gray 1000": {
+    "value": "#232422",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#232422",
+      "type": "color"
+    },
+    "name": "Gray1000",
+    "attributes": {
+      "category": "Gray 1000"
+    },
+    "path": [
+      "Gray 1000"
+    ]
+  },
+  "Gray 800": {
+    "value": "#60615F",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#60615F",
+      "type": "color"
+    },
+    "name": "Gray800",
+    "attributes": {
+      "category": "Gray 800"
+    },
+    "path": [
+      "Gray 800"
+    ]
+  },
+  "Gray 600": {
+    "value": "#DEE0DA",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#DEE0DA",
+      "type": "color"
+    },
+    "name": "Gray600",
+    "attributes": {
+      "category": "Gray 600"
+    },
+    "path": [
+      "Gray 600"
+    ]
+  },
+  "Gray 400": {
+    "value": "#EFF0ED",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#EFF0ED",
+      "type": "color"
+    },
+    "name": "Gray400",
+    "attributes": {
+      "category": "Gray 400"
+    },
+    "path": [
+      "Gray 400"
+    ]
+  },
+  "Gray 200": {
+    "value": "#F4F5F2",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#F4F5F2",
+      "type": "color"
+    },
+    "name": "Gray200",
+    "attributes": {
+      "category": "Gray 200"
+    },
+    "path": [
+      "Gray 200"
+    ]
+  },
+  "font-size-10": {
+    "value": "10px",
+    "type": "fontSizes",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "10px",
+      "type": "fontSizes"
+    },
+    "name": "FontSize10",
+    "attributes": {
+      "category": "font-size-10"
+    },
+    "path": [
+      "font-size-10"
+    ]
+  },
+  "font-size-12": {
+    "value": "12px",
+    "type": "fontSizes",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "12px",
+      "type": "fontSizes"
+    },
+    "name": "FontSize12",
+    "attributes": {
+      "category": "font-size-12"
+    },
+    "path": [
+      "font-size-12"
+    ]
+  },
+  "font-size-14": {
+    "value": "14px",
+    "type": "fontSizes",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "14px",
+      "type": "fontSizes"
+    },
+    "name": "FontSize14",
+    "attributes": {
+      "category": "font-size-14"
+    },
+    "path": [
+      "font-size-14"
+    ]
+  },
+  "font-size-16": {
+    "value": "16px",
+    "type": "fontSizes",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "16px",
+      "type": "fontSizes"
+    },
+    "name": "FontSize16",
+    "attributes": {
+      "category": "font-size-16"
+    },
+    "path": [
+      "font-size-16"
+    ]
+  },
+  "font-size-20": {
+    "value": "20px",
+    "type": "fontSizes",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "20px",
+      "type": "fontSizes"
+    },
+    "name": "FontSize20",
+    "attributes": {
+      "category": "font-size-20"
+    },
+    "path": [
+      "font-size-20"
+    ]
+  },
+  "font-size-24": {
+    "value": "24px",
+    "type": "fontSizes",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "24px",
+      "type": "fontSizes"
+    },
+    "name": "FontSize24",
+    "attributes": {
+      "category": "font-size-24"
+    },
+    "path": [
+      "font-size-24"
+    ]
+  },
+  "font-size-32": {
+    "value": "32px",
+    "type": "fontSizes",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "32px",
+      "type": "fontSizes"
+    },
+    "name": "FontSize32",
+    "attributes": {
+      "category": "font-size-32"
+    },
+    "path": [
+      "font-size-32"
+    ]
+  },
+  "font-family-base": {
+    "value": "\"Roboto\",\"Hiragino Sans\",\"Hiragino Kaku Gothic ProN\",\"YuGothic\"",
+    "type": "fontFamilies",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "\"Roboto\",\"Hiragino Sans\",\"Hiragino Kaku Gothic ProN\",\"YuGothic\"",
+      "type": "fontFamilies"
+    },
+    "name": "FontFamilyBase",
+    "attributes": {
+      "category": "font-family-base"
+    },
+    "path": [
+      "font-family-base"
+    ]
+  },
+  "opacity-20": {
+    "value": 0.2,
+    "type": "opacity",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": 0.2,
+      "type": "opacity"
+    },
+    "name": "Opacity20",
+    "attributes": {
+      "category": "opacity-20"
+    },
+    "path": [
+      "opacity-20"
+    ]
+  },
+  "opacity-50": {
+    "value": 0.5,
+    "type": "opacity",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": 0.5,
+      "type": "opacity"
+    },
+    "name": "Opacity50",
+    "attributes": {
+      "category": "opacity-50"
+    },
+    "path": [
+      "opacity-50"
+    ]
+  },
+  "opacity-80": {
+    "value": 0.8,
+    "type": "opacity",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": 0.8,
+      "type": "opacity"
+    },
+    "name": "Opacity80",
+    "attributes": {
+      "category": "opacity-80"
+    },
+    "path": [
+      "opacity-80"
+    ]
+  },
+  "border-width-1": {
+    "value": 1,
+    "type": "borderWidth",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": 1,
+      "type": "borderWidth"
+    },
+    "name": "BorderWidth1",
+    "attributes": {
+      "category": "border-width-1"
+    },
+    "path": [
+      "border-width-1"
+    ]
+  },
+  "border-width-2": {
+    "value": 2,
+    "type": "borderWidth",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": 2,
+      "type": "borderWidth"
+    },
+    "name": "BorderWidth2",
+    "attributes": {
+      "category": "border-width-2"
+    },
+    "path": [
+      "border-width-2"
+    ]
+  },
+  "border-width-3": {
+    "value": 3,
+    "type": "borderWidth",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": 3,
+      "type": "borderWidth"
+    },
+    "name": "BorderWidth3",
+    "attributes": {
+      "category": "border-width-3"
+    },
+    "path": [
+      "border-width-3"
+    ]
+  },
+  "border-width-4": {
+    "value": 4,
+    "type": "borderWidth",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": 4,
+      "type": "borderWidth"
+    },
+    "name": "BorderWidth4",
+    "attributes": {
+      "category": "border-width-4"
+    },
+    "path": [
+      "border-width-4"
+    ]
+  },
+  "tokenSetOrder": {
+    "0": {
+      "value": "global",
+      "type": "other",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "global",
+        "type": "other"
+      },
+      "name": "TokenSetOrder0",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "0"
+      },
+      "path": [
+        "tokenSetOrder",
+        "0"
+      ]
+    }
+  }
+};
